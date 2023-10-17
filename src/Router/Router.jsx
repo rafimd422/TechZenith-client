aa5d52037d14657f679162ff0b6cee9ce98d5091
rafimd422 <rafimf2222@gmail.com>
2023-10-17 23:59:22 +0600

    added basic router setup

---
+++ src/Router/Router.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom';
+import Root from '../components/layout/Root';
+import Home from '../components/Pages/Home/Home';
+import SignIn from '../components/Pages/SignIn/SignIn';
+import SignUp from '../components/Pages/SignUp/SignUp';
+
+const Router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root/>,
+      // errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/signin",
+          element: <SignIn />,
+        },
+        {
+          path: "/signup",
+          element: <SignUp />,
+        },
+      ],
+    },
+  ]);
+
+export default Router
---
+++ src/components/layout/Root.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom"
+import Nav from './../Fixed/Nav';
+import Footer from "../Fixed/Footer";
+
+const Root = () => {
+  return (<>
+    <div>
+      <Nav />
+<Outlet/>
+ <Footer />
+    </div>
+    </>
+  )
+}
+
+export default Root
