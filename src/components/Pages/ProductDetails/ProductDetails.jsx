3d2dc7799c6c529991dd0fcbbe6cc50e37b96772
rafimd422 <rafimf2222@gmail.com>
2023-10-19 23:06:02 +0600

    complated a post request to our database, to access specific data on our cart, whenever user click on the cart

--- src/components/Pages/ProductDetails/ProductDetails.jsx
+++ src/components/Pages/ProductDetails/ProductDetails.jsx
@@ -3,7 +3,32 @@ import { useLoaderData } from "react-router-dom"
 
 const ProductDetails = () => {
     const data = useLoaderData()
-console.log(data)
+
+
+    const addToCart = () => {
+      const { brand, category, name, price, rating, photourl } = data;
+      const newObject = { brand, name, category, price, photourl, rating };
+    
+      fetch('http://localhost:5000/cart', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json', // Fixed header
+        },
+        body: JSON.stringify(newObject),
+      })
+        .then((res) => res.json())
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    };
+    
+
+
+
+
 
   return (
     <div className="lg:container lg:mx-auto md:px-0 px-12 flex flex-wrap gap-6 p-2 items-center justify-center">
@@ -15,7 +40,7 @@ console.log(data)
     <h3 className="text-2xl font-bold">
         {data.name}
     </h3>
-    <button className={data.avaiablity === "Available" ? `bg-green-500 mb-2 hover:bg-green-700 text-white font-bold py-2 px-4 rounded` : `bg-red-500 hover:bg-red-700 text-white font-bold ms-auto mb-2 py-2 px-4 rounded`}>
+    <button className={data.avaiablity === "Available" ? `bg-green-500 mb-2 cursor-default hover:bg-green-700 text-white font-bold py-2 px-4 rounded` : `bg-red-500 hover:bg-red-700 text-white font-bold ms-auto mb-2 py-2 cursor-default px-4 rounded`}>
     {data.avaiablity}
     </button>
     </div>
@@ -24,8 +49,9 @@ console.log(data)
     </p>
 <div className="flex justify-between items-center px-4">    
 <h3 className="font-bold text-2xl text-red-500">Price: ${data.price}</h3>
-<button
-                  className="px-5 py-2 transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none">Add
+                <button
+                onClick={addToCart}
+                  className="px-5 py-2 bg-red-600 text-white font-extrabold transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none">Add
             to cart</button>
 </div>
 </div>
